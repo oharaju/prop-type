a5680280d4f5c0ed103437dce2df57abd64cb013
Júlia Ohara <juliaohara.br@gmail.com>
2022-12-27 14:37:41 -0300

    feat: add new list

--- src/components/CadastroPessoa/index.jsx
+++ src/components/CadastroPessoa/index.jsx
@@ -4,7 +4,8 @@ export const CadastroPessoa = () => (
   <div>
     <h1>Olá</h1>
     <ul>
-      <DadosPessoa nome="Júlia" ano_nascimento="1999" cidade="Olinda" />
+      <DadosPessoa nome="Mateus" ano_nascimento="1997" cidade="São Paulo" />
+      <DadosPessoa nome="Luana" ano_nascimento="1999" cidade="Rio de janeiro" />
       <DadosPessoa />
     </ul>
   </div>
